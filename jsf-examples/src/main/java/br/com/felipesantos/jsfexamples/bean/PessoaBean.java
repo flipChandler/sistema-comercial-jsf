8937047a4ce1e527c291798e70525b0b6cde0751
Felipe Santos <felipe.santos11@hotmail.com>
2024-04-01 21:37:22 -0300

    feat: jsf examples

---
+++ jsf-examples/src/main/java/br/com/felipesantos/jsfexamples/bean/PessoaBean.java
@@ -0,0 +1,44 @@
+package br.com.felipesantos.jsfexamples.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "pessoaBean")
+public class PessoaBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nome;
+	private String sobrenome;
+	private String nomeCompleto;
+	
+	public void mostrarNomeCompleto() {
+		nomeCompleto = nome + " " + sobrenome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+}
